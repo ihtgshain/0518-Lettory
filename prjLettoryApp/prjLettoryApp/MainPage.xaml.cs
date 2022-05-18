dc0c740d7814d0fafcb10e03f678b0d8fca4911e
張正昱 <ihtgschen@gmail.com>
2022-05-18 12:35:25 +0800

    加入專案檔案。

---
+++ prjLettoryApp/prjLettoryApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using prjLettoryApp.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace prjLettoryApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        List<Label> listLb = new List<Label>();
+        Lottery loNums;
+        public void DrawLottery(object sender, EventArgs e)
+        {
+            loNums =new Lottery();
+            ShowNums();
+        }
+
+        public void Sort(object sender, EventArgs e)
+        {
+            if (listLb.Count == 0) return;
+
+            loNums.BubbleSort();
+            ShowNums();
+        }
+
+        private void ShowNums()
+        {
+            listLb.Clear();
+            stackLettory.Children.Clear();
+            labInfo.Text = loNums.isSorted? "(Sorted)" : "(original order)";
+            labInfo.TextColor = Color.Black;
+            for (int i = 0; i < loNums.len; i++)
+            {
+                listLb.Add(new Label());
+                listLb[i].Text = loNums[i];
+                listLb[i].FontSize = 20;
+                stackLettory.Children.Add(listLb[i]);
+            }
+        }
+
+    }
+
+  
+}
---
+++ prjLettoryApp/prjLettoryApp/model/Lottery.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prjLettoryApp.model
+{
+    internal class Lottery
+    {
+        private int[] _nums = new int[6];
+        private bool _sorted = false;
+        Random rd = new Random();
+
+        public bool isSorted{get=>_sorted;}
+        public int len { get => _nums.Length; }
+
+        public Lottery()
+        {
+            for(int i = 0; i < _nums.Length; i++)
+            {
+                int temp = rd.Next(1, 50);
+                if (Array.IndexOf(_nums, temp) == -1)
+                    _nums[i] = temp;
+                else
+                    i--;
+            }
+        }
+
+        public string this [int index]
+        {
+            get
+            {
+                if (index >= 0 && index < _nums.Length)
+                {
+                    int result = _nums[index];
+                    return result < 10 ? $"0{result}" : $"{result}";
+                }
+                else return "";
+            }
+        }
+
+        public void BubbleSort()
+        {
+            if (_sorted)    return;
+
+            for (int i = 0; i < _nums.Length; i++)
+            {
+                for (int j = i + 1; j < _nums.Length; j++)
+                {
+                    if (_nums[i] > _nums[j])
+                    {
+                        int big = _nums[i];
+                        _nums[i] = _nums[j];
+                        _nums[j] = big;
+                    }
+                }
+            }
+            _sorted = true;
+        }
+    }
+}
